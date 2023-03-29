183ab1c4287d7105111ab88a28fa707c11a63b98
Jon Crosby <jonathanccrosby214@gmail.com>
2023-03-29 22:40:09 +0000

    More changes

--- QuizSite/QuizSite/settings.py
+++ QuizSite/QuizSite/settings.py
@@ -29,10 +29,7 @@ SECRET_KEY = get_random_secret_key()
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'localhost',
-    '104.248.224.128',
-    '.quizbox.app'
+    '*'
     ]
 
 # Application definition
