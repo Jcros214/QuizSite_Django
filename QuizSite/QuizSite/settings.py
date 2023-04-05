da9a213776389387d1cd8e27b61c23afc75b7c91
Jon <jcros513@students.bju.edu>
2023-04-04 23:08:08 -0400

    ALLOWED_HOSTS update

--- QuizSite/QuizSite/settings.py
+++ QuizSite/QuizSite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = get_random_secret_key()
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
-    '*'
+    '127.0.0.1','161.35.252.20', 'www.quizbox.app', 'quizbox.app'
     ]
 
 # Application definition
