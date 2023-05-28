4d58a3245bdd89aaa2c53538a69e6b265af051b9
Jon <jcros513@students.bju.edu>
2023-05-27 23:57:50 -0400

    Minor changes to new round robin Added as working.

--- QuizSite/TournamentManager/round_robin_scheduler.py
+++ QuizSite/TournamentManager/round_robin_scheduler.py
@@ -1,6 +1,7 @@
 from random import choice
 from typing import Tuple, List
 
+LETTERS = [chr(_) for _ in range(65, 65 + 26)]
 
 class RoundRobinScheduler:
     matches = set()
@@ -9,18 +10,19 @@ class RoundRobinScheduler:
     rounds = set()
     full_teams = []
 
-    def __init__(self, number_teams: int = 18, round_number: int = 4, matches_per_round: int = 5):
+    def __init__(self, number_teams: int = 18, round_number: int = 12, matches_per_round: int = 4):
         self.teams = set(range(number_teams))
         self.round_number = round_number
         self.matches_per_round = matches_per_round
 
     @staticmethod
-    def create_schedule(team_names: Tuple[str], number_teams: int = 18, round_number: int = 4,
-                        matches_per_round: int = 5):
+    def create_schedule(team_names: Tuple[str], number_teams: int = 18, round_number: int = 12,
+                        matches_per_round: int = 4):
         scheduler = RoundRobinScheduler(number_teams, round_number, matches_per_round)
         scheduler.create_matches()
         scheduler.give_teams_names(team_names)
-        scheduler.create_rounds()
+        if not scheduler.rounds:
+            scheduler.create_rounds()
         return scheduler.rounds
 
     def get_matchable_team(self, team_num: int) -> int:
@@ -37,8 +39,8 @@ class RoundRobinScheduler:
         for team in self.teams:
             team_match_key = f"team{team}"
             team_count_key = f"team{team}-count"
-            self.team_stats[team_match_key]: list = [team]
-            self.team_stats[team_count_key]: int = 0
+            self.team_stats[team_match_key] = [team]
+            self.team_stats[team_count_key] = 0
 
         for team in self.teams:
             main_team_count_key = f"team{team}-count"
@@ -91,16 +93,95 @@ class RoundRobinScheduler:
                 matches.remove(match_to_add)
                 matches_for_round.append(tuple(match_to_add))
 
+                if k < self.matches_per_round - 1 and len(matches) == 0: 
+                    # if we're not on the last match and there are no more matches to add 
+                    # in other words, we've run out of matches to try
+                    return False
             self.rounds.add(tuple(matches_for_round))
 
 
+
+def tabulate_rounds(schedule, rooms_letters: list[str]) -> dict[str, list[str]]:
+    num_rounds = len(schedule)
+    
+    rooms = {letter:[] for letter in rooms_letters}
+
+    # convert from tuples to list
+    sch = [list(list(sc) for sc in s) for s in schedule]
+
+    for room in list(rooms.keys()):
+        for rnd in sch:
+            rooms[room].append(rnd.pop(0))
+        
+
+        # rooms[].append(rnd.pop())
+
+
+    # rooms = {letter:[schedule.pop() for _ in range(7)] for letter in LETTERS[:7]}
+
+    output = []
+    header = "|   |" + "|".join([f"{'Round ' + str(i+1):^14}" for i in range(num_rounds)]) + "|"
+    output.append(header)
+    output.append("|---|" + "|".join(["-"*14] * num_rounds) + "|")
+
+    for room in rooms:
+        rnd = [f"|{room:^3}|"]
+        for quiz in rooms[room]:
+            rnd.append(f"{' v '.join(quiz):^14}|")
+        output.append("".join(rnd))
+
+    print("\n".join(output))
+
+    return rooms
+
+def get_all_quizzes_from_schedule(schedule: dict[str, list[str]]) -> list[str]:
+    quizzes = []
+    for team in schedule:
+        for quiz in schedule[team]:
+            if quiz not in quizzes:
+                quizzes.append(quiz)
+            else:
+                raise ValueError(f"Quiz {quiz} already in list")
+    return quizzes
+
+def find_fairness(teams: list[str], schedule: dict[str, list[str]]) -> dict[str, dict[str, int]]:
+    fairness_dict = {}
+
+    for team in teams:
+        fairness_dict[team] = {team:0 for team in teams}
+
+        for quiz in get_all_quizzes_from_schedule(schedule):
+            if team in quiz:
+                for quiz_team in quiz:
+                    fairness_dict[team][quiz_team] += 1
+
+    
+    return fairness_dict
+
+
+def main():
+    teams = [f'R{_}' for _ in LETTERS[:18]]
+
+    teamsets = [[f'{div}{_}' for _ in LETTERS[:18]] for div in ['R', 'G']]
+    try:
+        schedules = [RoundRobinScheduler.create_schedule(teams) for teams in teamsets]
+
+        for schedule in schedules:
+
+            dict_schedule = tabulate_rounds(schedule, LETTERS[:4])
+
+            print(find_fairness(teams, dict_schedule), '\n\n')
+    
+        return True
+
+    except Exception as e:
+        print(e)
+        
+
+
+
+
+
 if __name__ == "__main__":
-    teams = (
-    "team1", "team2", "team3", "team4", "team5", "team6", "team7", "team8", "team9", "team10", "team11", "team12",
-    "team13", "team14", "team15", "team16", "team17", "team18")
-    print(RoundRobinScheduler.create_schedule(teams))
-
-
-
-
-
+    while not main():
+        main()
