1111cab46971ced4cc15556aef0b48b8ca1f533f
Jon <jcros513@students.bju.edu>
2023-06-30 01:26:49 -0400

    changed static_files

--- QuizSite/QuizSite/settings.py
+++ QuizSite/QuizSite/settings.py
@@ -168,7 +168,7 @@ STATICFILES_DIRS = [
     # '/var/www/static/',
 ]
 
-STATIC_ROOT = '/var/www/quizbox.app/static/'
+STATIC_ROOT = 'collected_static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
