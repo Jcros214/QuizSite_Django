a1a28ca4d1fc94e89c53229243b01eea75fc30a5
Jon <jcros513@students.bju.edu>
2023-07-09 18:40:05 -0400

    removed the schedule from event view

--- Records/models.py
+++ Records/models.py
@@ -175,57 +175,8 @@ class Event(models.Model):
         return self.get_current_round() + 1 if self.get_current_round() else None
 
     def get_event_view_data(self):
-        raw_query = f'''
-            select
-            t.short_name,
-            t.name as teamname,
-            t.division as division,
-            cr.id as currentround,
-            nr.id as nextround,
-            i.id as individualid,
-            i.name as individualname,
-            sum(coalesce(aq.value, 0)) as points,
-            cr2.attendancepoints as attendancepoints
-        from "Records_team" t
-        left join "Records_teammembership" tm on tm.team_id = t.id
-        left join "Records_individual" i on tm.individual_id = i.id
-        left join "Records_quizparticipants" qp on qp.team_id = t.id
-        left join "Records_quiz" rq on qp.quiz_id = rq.id
-        left join "Records_event" e on rq.event_id = e.id
-        left join "Records_askedquestion" aq on aq.individual_id = i.id and aq.quiz_id = rq.id
-        left join (
-            SELECT qp.team_id, rqc.id
-            from "Records_quizparticipants" qp
-            left join "Records_quiz" rqc on qp.quiz_id = rqc.id
-            where cast(rqc.round as int) = (select min(cast(rq1.round as int)) from "Records_quiz" rq1 where rq1."isValidated" = false)
-        ) as cr on cr.team_id = t.id
-        left join (
-            SELECT qp.team_id, rqn.id
-            from "Records_quizparticipants" qp
-            left join "Records_quiz" rqn on qp.quiz_id = rqn.id
-            where cast(rqn.round as int) = ((select min(cast(rq2.round as int)) from "Records_quiz" rq2 where rq2."isValidated" = false) + 1)
-        ) as nr on nr.team_id = t.id
-        left join (
-            SELECT qp.team_id, count(qp.team_id)*20 as attendancepoints
-            from "Records_quizparticipants" qp
-            left join "Records_quiz" rqc2 on qp.quiz_id = rqc2.id
-            where cast(rqc2.round as int) <= (select min(cast(rq3.round as int)) from "Records_quiz" rq3 where rq3."isValidated" = false)
-            group by qp.team_id
-        ) as cr2 on cr2.team_id = t.id
-        where e.id = {self.pk}
-        group by
-            t.short_name,
-            t.name,
-            t.division,
-            cr.id,
-            nr.id,
-            i.id,
-            i.name,
-            cr2.attendancepoints
-        order by t.short_name, i.id
-
-
-                '''
+        with open('Records/queries/Event View.pgsql', 'r') as f:
+            raw_query = f.read().replace('{}', str(self.pk))
 
         teams = []
 
